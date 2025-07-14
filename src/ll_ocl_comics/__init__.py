366144f480046d021f350609a0f0665f709b4421
KyberCritter <coding@scottratchford.com>
2025-07-13 21:04:17 -0500

    added JS to init

--- src/ll_ocl_comics/__init__.py
+++ src/ll_ocl_comics/__init__.py
@@ -6,3 +6,11 @@ from .apis import (
 from .app import (
     MokuroTranslator,
 )
+
+from .mokuro_changes import (
+    PROPERTIES_JS_FUNC,
+    LISTENER_JS_FUNC,
+    ALWAYS_SHOW_TRANSLATION_JS_FUNC,
+    UPDATE_PAGE_JS_ORIGINAL,
+    UPDATE_PAGE_JS_FUNC,
+)
